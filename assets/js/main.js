1c0311d39389d4ca390663fb325407b45cd93d71
Ronald Silva <ronald.creative@gmail.com>
2022-08-23 21:54:51 -0300

    ajustes de layout e adicionando media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,40 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id'),
+    sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId +']');
+  
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      sectionsClass.classList.add('active-link')
+    }else{
+      sectionsClass.classList.remove('active-link')
+    }
+  
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
 
 /*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+
+  // quando a rolagem  for maior que 350 viewport de altura, 
+  // adicionar a classe show-scroll  para a tag com scrollup
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+  
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
